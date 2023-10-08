cb0f1f0209eef86c4fb847738613e7e37b40b06f
Illia Fialkivskiji <fialkivskiji@gmail.com>
2023-10-08 19:07:07 +0300

    context

--- client/src/pages/flight/Flight.jsx
+++ client/src/pages/flight/Flight.jsx
@@ -4,8 +4,8 @@ import useFetch from '../../hooks/useFetch';
 
 const Flight = () => {
     const location = useLocation()
-    const id = location.split("/"[2])
-    const {data, loading, error} = useFetch(`/flights/${id}`)
+    const id = location.pathname.split("/")[2]
+    const {data, loading, error} = useFetch(`/flights/find/${id}`)
 
     return (
         <div>
