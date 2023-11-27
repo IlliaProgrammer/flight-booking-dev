3c4039afcd2c5947a4663e39bb17af03c997baf3
Illia Fialkivskiji <fialkivskiji@gmail.com>
2023-11-27 22:49:58 +0200

    async mail

--- api/controllers/mailController.js
+++ api/controllers/mailController.js
@@ -1,58 +1,55 @@
-import nodemailer from "nodemailer"
- 
+import nodemailer from "nodemailer";
 
-
-const mailer  = message => {
+const mailer = async (message) => {
+  try {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }},
-        {
-            from: 'SkyWings Express <SkyWingsExpress@gmail.com>',
-        }
-    )
-    transporter.sendMail(message, (err, info) => {
-        if(err) return console.log(err)
-        console.log('Email sent: ', info)
-    })
-}
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+      }
+    });
 
+    const info = await transporter.sendMail(message);
+    console.log('Email sent: ', info);
+  } catch (err) {
+    throw err; 
+  }
+};
 
-export const send = (req, res, next) =>{
-    try{
-        if(!req.body.email ) return res.sendStatus(400)   
-        const message = {        
-            to: req.body.email,
-            subject: 'Congratulations! You are successfully registred on our site',
-            text: `Congratulations! You are successfully registred on our site
+export const send = async (req, res, next) => {
+  try {
+    if (!req.body.email) return res.sendStatus(400);
+    const message = {
+      to: req.body.email,
+      subject: 'Congratulations! You are successfully registered on our site',
+      text: `Congratulations! You are successfully registered on our site
             login: ${req.body.email}
             no reply`
-        }
-    mailer(message)
-    res.status(200).send("done") 
-    }catch(err){
-        next(err)
-    }
-}
+    };
 
+    await mailer(message);
+    res.status(200).send("done");
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const contact = (req, res, next) =>{
-    try{
-        if(!req.body.email || !req.body.text) return res.sendStatus(400)   
-        const message = {        
-            to: process.env.EMAIL,
-            subject: 'Feedback from client',
-            text: `From login: ${req.body.email}
-            Text: ${req.body.text}
-           `
-        }
-    mailer(message)
-    res.status(200).send("done") 
-    }catch(err){
-        next(err)
-    }
-}
+export const contact = async (req, res, next) => {
+  try {
+    if (!req.body.email || !req.body.text) return res.sendStatus(400);
+    const message = {
+      to: process.env.EMAIL,
+      subject: 'Feedback from client',
+      text: `From login: ${req.body.email}
+            Text: ${req.body.text}`
+    };
+
+    await mailer(message);
+    res.status(200).send("done");
+  } catch (err) {
+    next(err);
+  }
+};
